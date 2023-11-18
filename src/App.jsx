014fe46e24058b03275d4cea5bf79092e4f2168d
Rodrigo Velasco <rorito125@gmail.com>
2023-11-18 12:50:52 -0400

    comparacion de pixelarts completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import { PixelCopy } from "./games/PixelCopy"
 
 
 
 function App() {
+  const [playerScore, setPlayerScore] = useState(0);
+
   return (
     <div className="container" >
-      <PixelCopy/>
+      <h1>Score: {playerScore}</h1>
+      <PixelCopy setPlayerScore={setPlayerScore}/>
     </div>
   )
 }
--- src/games/PixelCopy.jsx
+++ src/games/PixelCopy.jsx
@@ -1,66 +1,82 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import audioNext from '../assets/sound-next.mp3';
 
-const Pixel = ({value, r, c, pixels, setPixels, block=false}) => {
-
-
+const Pixel = ({value, r, c, pixels, setPixels, block=false, pixelArtsEquals, pixelsEquals=false}) => {
   const handleClick = (e) => {
     if(e.buttons === 1){
       const newPixels = pixels;
       newPixels[r][c] = (newPixels[r][c] === 1)?0:1;
       setPixels([...newPixels]);
+      pixelArtsEquals()
     }
   }
   return(
     <button 
-      className= {`pixel ${(value===0)?'pixel-btn-1':'pixel-btn-2'}`}
+      className= {`pixel ${(value===0)?'pixel-btn-1':'pixel-btn-2'} ${pixelsEquals&&'equals'}`}
       onMouseEnter={handleClick}
       onMouseDown={handleClick}
-      disabled={block}
+      disabled={block || pixelsEquals}
     ></button>
   )
 }
 
-export const PixelCopy = () => {
+export const PixelCopy = ({setPlayerScore}) => {
 
-  const [pixels, setPixels] = useState([
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0]
-                                      ]);
-  const [copyPixel, setcopyPixel] = useState(pixels);
-  const [score, setScore] = useState(0)
-
-  
+  const [pixelsEquals, setPixelsEquals] = useState(false);
+  const initialStatePixels = [
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0,0]
+                            ]
+  const [pixels, setPixels] = useState([...initialStatePixels]);
+  const [copyPixel, setcopyPixel] = useState([...initialStatePixels]);
+  const [score, setScore] = useState(0);
+    
   const savePixelArt = async() => {
-    let score = 0;
+    // let score = 0;
 
-    for (let i = 0; i < pixels.length; i++) {
-      pixels[i].forEach(p => {
-        if(p===1) score++;
-      })
-    }
+    // for (let i = 0; i < pixels.length; i++) {
+    //   pixels[i].forEach(p => {
+    //     if(p===1) score++;
+    //   })
+    // }
 
-    try {
-      const url = 'http://localhost:8080/api/pixelcopy';
-      const data = {pixels: JSON.stringify(pixels), score};
+    // try {
+    //   const url = 'http://localhost:8080/api/pixelcopy';
+    //   const data = {pixels: JSON.stringify(pixels), score};
 
-      const response = await axios.post(url, data);
+    //   const response = await axios.post(url, data);
       
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
-
+    //   console.log(response)
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
 
-  useEffect(() => {
+  const pixelArtsEquals = () => {
+    const isEqueals = JSON.stringify(pixels) === JSON.stringify(copyPixel);
+    if(isEqueals) {
+      const next = new Audio(`${audioNext}`);
+      next.play();
+      setPixelsEquals(true);
+      setPlayerScore(s=>s+score);
+
+      setTimeout(() => { // Reset pixels
+        setPixelsEquals(false);
+        setPixels(initialStatePixels);
+        newPixelArt();
+      }, 1000);
+    }
+  }
+
+  const newPixelArt = () => {
     axios.get('http://localhost:8080/api/pixelcopy/random')
       .then(respose=>{
         const {data} = respose;
@@ -68,19 +84,22 @@ export const PixelCopy = () => {
         setScore(data.pixelart.score);
       })
       .catch(error=>console.log(error))
+  }
+
+  useEffect(() => {
+    newPixelArt();
   }, [])
   
 
 
   return (
     <div className='game-container-px' >
-
       <div style={{display:'inline-block' }} >
         {pixels.map((row,r)=>(
           <div key={r}>
             {
               row.map((value,c) => (
-                <Pixel key={c} value={value} r={r} c={c} pixels={pixels} setPixels={setPixels} />
+                <Pixel key={c} value={value} r={r} c={c} pixels={pixels} setPixels={setPixels} pixelArtsEquals={pixelArtsEquals} pixelsEquals={pixelsEquals} />
               ))
             }
           </div>
@@ -95,7 +114,7 @@ export const PixelCopy = () => {
           <div key={r}>
             {
               row.map((value,c) => (
-                <Pixel key={c} value={value} r={r} c={c} pixels={pixels} setPixels={setPixels} block />
+                <Pixel key={c} value={value} r={r} c={c} pixels={pixels} setPixels={setPixels} block pixelsEquals={pixelsEquals} />
               ))
             }
           </div>
